da3e09415b27b2b96dba3240dd6e4f697bf0450c
ChrisXLVII <fogas.krisztian@gmail.com>
2024-12-02 19:35:20 +0100

    day 2 part 2

--- day2/day2_nuclear_fusion.py
+++ day2/day2_nuclear_fusion.py
@@ -25,15 +25,29 @@ def is_safe(numbers: list[int]) -> bool:
             return False
     return True
 
+def is_safe_dampened(numbers: list[int]) -> bool:
+    for count, _ in enumerate(numbers):
+        if is_safe(numbers[:count] + numbers[count+1:]):
+            return True
+    return False
 
 def main():
     with open(r"day2\input") as f:
         lines = f.read().splitlines()
 
+    # First part:
     count = 0
     for line in lines:
         numbers = list(map(int, line.split()))
         if is_safe(numbers):
+            count += 1
+    print(count)
+    
+    # Second part:
+    count = 0
+    for line in lines:
+        numbers = list(map(int, line.split()))
+        if is_safe_dampened(numbers):
             count += 1
     print(count)
 
