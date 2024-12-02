a4df42076b1496cddd3746f43042675f502325b8
Kristián Fogas <kfogas@szintezis-net.hu>
2024-12-02 17:28:00 +0100

    day 2 part 1 solution

---
+++ day2/day2_nuclear_fusion.py
@@ -0,0 +1,42 @@
+
+def is_ok_distance(first: int, second: int) -> bool:
+    if first > second:
+        distance = first - second
+    else:
+        distance = second - first
+    if distance > 3 or distance == 0:
+        return False
+    return True
+
+
+def is_safe(numbers: list[int]) -> bool:
+    increasing = None
+    decreasing = None
+    for count, value in enumerate(numbers[1:], start=1):
+        if value > numbers[count-1]:
+            if decreasing:
+                return False
+            increasing = True
+        else:
+            if increasing:
+                return False
+            decreasing = True
+        if not is_ok_distance(numbers[count-1], value):
+            return False
+    return True
+
+
+def main():
+    with open(r"day2\input") as f:
+        lines = f.read().splitlines()
+
+    count = 0
+    for line in lines:
+        numbers = list(map(int, line.split()))
+        if is_safe(numbers):
+            count += 1
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
