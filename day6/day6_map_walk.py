7080c2ae3f6b40eaa88e63e96de31727ac32c3a3
Fogas Kristián <kfogas@brighthills.com>
2024-12-07 13:33:17 +0100

    tidy

--- day6/day6_map_walk.py
+++ day6/day6_map_walk.py
@@ -40,21 +40,21 @@ def walk(input_map, start):
         # Check if not out of bounds
         if next_step[0] < 0 or next_step[1] < 0 or next_step[0] >= len(input_map) or next_step[1] >= len(input_map[0]):
             break
-        
+
         # Check if not "#"
         if input_map[next_step[0]][next_step[1]] == "#":
             direction = change_direction(direction)
             continue
         else:
-            
+
             # Do the actual step
             location = next_step
             visited.add(location)
-            
+
             location_with_vector = (location, direction)
             if location_with_vector in visited_with_dir:
                 return None
-                
+
             visited_with_dir.add((location, direction))
 
     return visited
@@ -64,29 +64,27 @@ def count_loops(input_map, start, possible_covered):
     loops = 0
     for visits in possible_covered:
         input_map[visits[0]][visits[1]] = '#'
-        
+
         if not walk(input_map, start):
             loops += 1
-        
+
         input_map[visits[0]][visits[1]] = '.'
-        
+
     return loops
 
 
 if __name__ == "__main__":
-    count_obstructions = 0
-    cycle_count = 0
     with open(r"day6\input") as f:
         lines = f.read().splitlines()
     input_map = [list(line) for line in lines]
 
     start = get_start(input_map, '^')
     visited = walk(input_map, start)
-    
+
     print(len(visited))
 
     loops = count_loops(input_map, start, visited)
-    
+
     print(loops)
 
     pass
--- day7/day7_math_bridge.py
+++ day7/day7_math_bridge.py
@@ -1,7 +1,9 @@
 from itertools import product
 
+
 def variations(elements, length):
     return [''.join(variation) for variation in product(elements, repeat=length)]
+
 
 def count_good_results(lines, operators):
     total_calibration_result = 0
@@ -10,11 +12,11 @@ def count_good_results(lines, operators):
         result = int(result)
         numbers = list(map(int, numbers.split()))
 
-        variants = variations(operators,len(numbers)-1)
+        variants = variations(operators, len(numbers)-1)
 
         for list_operators in variants:
             actual_result = 0
-            for number, operator in zip(numbers,' '+list_operators):
+            for number, operator in zip(numbers, ' '+list_operators):
                 if operator == ' ':
                     actual_result = number
                 if operator == '+':
@@ -29,6 +31,7 @@ def count_good_results(lines, operators):
                 total_calibration_result += result
                 break
     return total_calibration_result
+
 
 if __name__ == "__main__":
     with open(r"day7\input") as f:
