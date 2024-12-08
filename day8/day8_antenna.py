657a47de1e2074eacc514cb64518a25bafbc0ca2
Fogas Kristián <kfogas@brighthills.com>
2024-12-08 12:04:46 +0100

    day 8 part 2

--- day8/day8_antenna.py
+++ day8/day8_antenna.py
@@ -28,38 +28,57 @@ def out_of_bounds(antenna_map, position):
     return False
 
 
-def count_antinodes(antenna_pairs, antenna_map):
+def count_antinodes(antenna_map):
     count = 0
+    for row in antenna_map:
+        count += row.count("#")
+    return count
+
+
+def paint_map(antenna_pairs, antenna_map, part2=False):
     for antenna_pair in antenna_pairs:
+        i = 1
         diff_x = (antenna_pair[0][0] - antenna_pair[1][0])
         diff_y = (antenna_pair[0][1] - antenna_pair[1][1])
+        if part2:
+            antenna_map[antenna_pair[0][0]][antenna_pair[0][1]] = "#"
+            antenna_map[antenna_pair[1][0]][antenna_pair[1][1]] = "#"
 
-        antinode_position = (antenna_pair[0][0] + diff_x, antenna_pair[0][1] + diff_y)
-        if not out_of_bounds(antenna_map, antinode_position):
+        while True:
+            antinode_position = (antenna_pair[0][0] + (diff_x*i), antenna_pair[0][1] + (diff_y*i))
+            if not out_of_bounds(antenna_map, antinode_position):
 
-            # Debug
-            if antenna_map[antinode_position[0]][antinode_position[1]] == "#":
-                # print("Overlap!!!")
-                continue
+                antenna_map[antinode_position[0]][antinode_position[1]] = "#"
 
-            count += 1
-            antenna_map[antinode_position[0]][antinode_position[1]] = "#"
-
-    return count
+            else:
+                break
+            if part2:
+                i += 1
+            else:
+                break
 
 
 if __name__ == "__main__":
     antenna_map = []
-    count = 0
+    count_part_1 = 0
+    count_part_2 = 0
     with open(r"day8\input") as f:
         lines = f.read().splitlines()
     antenna_map = [list(line) for line in lines]
 
     unique_antennas = get_unique_antennas(antenna_map)
 
+    # Part 1
     for _, antennas in unique_antennas.items():
         antenna_pairs = get_antenna_pairs(antennas)
+        paint_map(antenna_pairs, antenna_map)
+        count_part_1 = count_antinodes(antenna_map)
 
-        count += count_antinodes(antenna_pairs, antenna_map)
+    # Part 2
+    for _, antennas in unique_antennas.items():
+        antenna_pairs = get_antenna_pairs(antennas)
+        paint_map(antenna_pairs, antenna_map, part2=True)
+        count_part_2 = count_antinodes(antenna_map)
 
-    print(count)
+    print(count_part_1)
+    print(count_part_2)
